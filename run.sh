486e8599f7e42acc0ba8922b34b3bacca96006f2
dddraxxx <dongqh078@gmail.com>
2025-01-28 22:14:20 -0800

    update

--- run.sh
+++ run.sh
@@ -32,7 +32,7 @@ case $DATASET in
 esac
 
 export MODEL_PATH="meta-llama/Llama-2-7b-hf"
-export SAVE_PATH='checkpoints/llama-2-7b-ft/'$DATASET
+export SAVE_PATH='checkpoints/'$DATASET'/llama-2-7b-ft'
 export MASTER_ADDR="localhost"
 # random port
 export MASTER_PORT=$(expr 10000 + $(od -An -N2 -i /dev/urandom) % 10000)
@@ -49,11 +49,11 @@ fi
 
 total_batch_size=128
 per_device_train_batch_size=2
-gradient_accumulation_steps=$((total_batch_size / per_device_train_batch_size))
 
 cuda_device=${CUDA_VISIBLE_DEVICES:-"1,2,3,4,5,6,7"}
+gradient_accumulation_steps=$((total_batch_size / per_device_train_batch_size / ${#cuda_device}))
 echo "using cuda device: $cuda_device"
-
+echo "gradient accumulation steps: $gradient_accumulation_steps"
 eval_only=${eval_only:-false}
 if [ "$eval_only" != "1" ]; then
     ddp_cmd="torchrun --master_addr ${MASTER_ADDR} --master_port ${MASTER_PORT} --nproc_per_node=${#cuda_device}"
@@ -61,26 +61,26 @@ if [ "$eval_only" != "1" ]; then
     args="train_math.py \
         --model_name_or_path $MODEL_PATH \
         --data_path $DATA_PATH \
-    --data_length 10000000 \
-    --bf16 True \
-    --output_dir $SAVE_PATH \
-    --num_train_epochs 3 \
-    --tune_layers $tune_layers \
-    --per_device_train_batch_size $per_device_train_batch_size \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps $gradient_accumulation_steps \
-    --evaluation_strategy no \
-    --save_strategy steps \
-    --save_steps 1000 \
-    --save_total_limit 2 \
-    --learning_rate 2e-5 \
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type cosine \
-    --logging_steps 3 \
-    --fsdp full_shard auto_wrap \
-    --fsdp_transformer_layer_cls_to_wrap LlamaDecoderLayer \
-    --tf32 True"
+        --data_length 10000000 \
+        --bf16 True \
+        --output_dir $SAVE_PATH \
+        --num_train_epochs 3 \
+        --tune_layers $tune_layers \
+        --per_device_train_batch_size $per_device_train_batch_size \
+        --per_device_eval_batch_size 4 \
+        --gradient_accumulation_steps $gradient_accumulation_steps \
+        --evaluation_strategy no \
+        --save_strategy steps \
+        --save_steps 1000 \
+        --save_total_limit 2 \
+        --learning_rate 2e-5 \
+        --weight_decay 0. \
+        --warmup_ratio 0.03 \
+        --lr_scheduler_type cosine \
+        --logging_steps 3 \
+        --fsdp full_shard auto_wrap \
+        --fsdp_transformer_layer_cls_to_wrap LlamaDecoderLayer \
+        --tf32 True"
     if [ ${#cuda_device} -gt 1 ]; then
         # echo "run: ${ddp_cmd} ${args}"
         CUDA_VISIBLE_DEVICES=${cuda_device} ${ddp_cmd} ${args}
