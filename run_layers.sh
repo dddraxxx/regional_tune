d2516fae403e5b7a47ddc3ce2054acda67a14448
dddraxxx <dongqh078@gmail.com>
2025-02-03 20:11:30 -0800

    updat

--- run_layers.sh
+++ run_layers.sh
@@ -1,23 +1,34 @@
 export data='metamath'
 export data='gsm8k'
-export WANDB_PROJECT='math_tune_layers'
+export WANDB_PROJECT='math_partial_metamath_tune_layers'
 wandb online
 
+export ep=1
+export dp=33
+
 ############### Layers ###############
-bash run.sh
-l=0-20 bash run.sh
-l=0-10 bash run.sh
-l=0-15 bash run.sh
+# bash run.sh
+# l=0-20 bash run.sh
+# l=0-10 bash run.sh
+# l=0-15 bash run.sh
 # l=0-25 bash run.sh
 # l=0-30 bash run.sh
 # l=10-31 bash run.sh
 
+# tune the continuous layers, 8 layers as a block
+for i in {0..31..8}; do
+    l=$0-$(($i+7)) bash run.sh
+    l=$31-$((31-i+1)) bash run.sh
+done
+
+l=8-23 bash run.sh
+
 # tune every 4 layers
-# for i in {0..31..4}; do
-#     l=$i-$(($i+3)) bash run.sh
-# done
+for i in {0..31..4}; do
+    l=$i-$(($i+3)) bash run.sh
+done
 
 # tune every 8 layers
-# for i in {0..31..8}; do
-#     l=$i-$(($i+7)) bash run.sh
-# done
+for i in {0..31..8}; do
+    l=$i-$(($i+7)) bash run.sh
+done
